bb36433d9f852469f7164e8df09cd9a215056187
PC02 <PC02@INFO5-5373>
2022-05-18 09:11:11 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_INF_CiuffredaLuigi_VerificaDiInf/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2122_INF_CiuffredaLuigi_VerificaDiInf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
